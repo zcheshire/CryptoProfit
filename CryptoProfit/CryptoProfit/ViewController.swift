6770944ec2ccedca9fde1b48202a4c536e5f2bd5
Zachary Cheshire <zcheshire3@gatech.edu>
2017-06-29 16:01:53 -0400

    Set up API call to get all tickers for ticker search

--- CryptoProfit/CryptoProfit/ViewController.swift
+++ CryptoProfit/CryptoProfit/ViewController.swift
@@ -30,20 +30,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        defaultTickers = model.getCurrentUser().getWatchList()
         model.refresh(tickers: model.getCurrentUser().getWatchList()  , base: "USD")
+        model.getTickers()
+        self.refresh()
         portfolioVal.text = "$0"
         model.getCurrentUser().addPosition(position: Position(coinType: "ETH", cryptoPrice: 360, positionAmount: 10.00528296, open: true))
         model.getCurrentUser().addPosition(position: Position(coinType: "ANS", cryptoPrice: 13 , positionAmount: 68.22148100, open: true))
         model.getCurrentUser().addPosition(position: Position(coinType: "SC", cryptoPrice: 0.02, positionAmount: 4029.53031144, open: true))
         model.getCurrentUser().addPosition(position: Position(coinType: "BTC", cryptoPrice: 0.02, positionAmount: 0.00000019, open: true))
-        
-
-        defaultTickers = model.getCurrentUser().getWatchList()
-        _ = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { (timer) in
-            model.refresh(tickers: model.getCurrentUser().getWatchList()  , base: "USD")
-            self.refresh()
-        }
-        
+    
         // Do any additional setup after loading the view, typically from a nib.
         
         //setting background color of entire canvas
@@ -106,7 +102,6 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         self.defaultTickers = []
         self.defaultPrices = []
         seperate = model.getData()
-        
         for (k, v) in seperate {
             self.defaultTickers.append(k)
             self.defaultPrices.append(v)
